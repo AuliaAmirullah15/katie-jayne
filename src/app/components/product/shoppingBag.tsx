bb6ac8424a802a38e722806940941e4fc832c957
Aulia Amirullah <auliaamir153@gmail.com>
2025-01-23 20:17:30 +0000

    Added scrolling to the shopping bag

--- src/app/components/product/shoppingBag.tsx
+++ src/app/components/product/shoppingBag.tsx
@@ -63,50 +63,53 @@ const ShoppingBag: React.FC<ShoppingBagProps> = ({
         <OverlayCloseButton onClick={onClose} />
         <h2 className="text-xl m-6">Shopping Bag</h2>
         <hr className="w-full border-t border-gray-300"></hr>
-        {basketItems.map((basketItem, index) => (
-          <div
-            className={`grid grid-cols-12 gap-6 m-6 pb-14 ${
-              index !== basketItems.length - 1
-                ? "border-b-2 border-gray-300"
-                : "border-0"
-            }`}
-            key={index}
-          >
-            <div className="col-span-4 flex flex-col relative">
-              <Image
-                src={basketItem.image}
-                alt={basketItem.name}
-                className="object-cover w-full max-h-[200px] h-full"
-                layout="fill"
-              />
-            </div>
-            <div className="col-span-7 flex flex-col">
-              <p className="text-xl mb-2">{basketItem.name}</p>
-              <p className="text-gray-700 text-lg mb-2">
-                {formatCurrency(basketItem.price, basketItem.currency)}
-              </p>
-              <QuantitySelector
-                quantity={basketItem.quantity}
-                onIncrement={() =>
-                  updateBasketItemQuantity(basketItem, "INCREMENT")
-                }
-                onDecrement={() =>
-                  updateBasketItemQuantity(basketItem, "DECREMENT")
-                }
-              />
-            </div>
-            <div className="col-span-1 flex flex-col">
-              <div
-                className="text-2xl cursor-pointer"
-                onClick={() => basketDispatch(removeFromBasket(basketItem))}
-              >
-                &times;
+
+        <div className="flex-1 grow overflow-y-auto">
+          {basketItems.map((basketItem, index) => (
+            <div
+              className={`grid grid-cols-12 gap-6 m-6 pb-8 ${
+                index !== basketItems.length - 1
+                  ? "border-b-2 border-gray-300"
+                  : "border-0"
+              }`}
+              key={index}
+            >
+              <div className="col-span-4 flex flex-col relative">
+                <Image
+                  src={basketItem.image}
+                  alt={basketItem.name}
+                  className="object-cover w-full max-h-[200px] h-full"
+                  layout="fill"
+                />
+              </div>
+              <div className="col-span-7 flex flex-col">
+                <p className="text-xl mb-2">{basketItem.name}</p>
+                <p className="text-gray-700 text-lg mb-2">
+                  {formatCurrency(basketItem.price, basketItem.currency)}
+                </p>
+                <QuantitySelector
+                  quantity={basketItem.quantity}
+                  onIncrement={() =>
+                    updateBasketItemQuantity(basketItem, "INCREMENT")
+                  }
+                  onDecrement={() =>
+                    updateBasketItemQuantity(basketItem, "DECREMENT")
+                  }
+                />
+              </div>
+              <div className="col-span-1 flex flex-col">
+                <div
+                  className="text-2xl cursor-pointer"
+                  onClick={() => basketDispatch(removeFromBasket(basketItem))}
+                >
+                  &times;
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
 
-        <div className="absolute bottom-0 left-0 w-full p-4 bg-white border-t border-gray-300">
+        <div className="left-0 w-full p-4 bg-white border-t border-gray-300">
           <p className="text-center text-gray-600 mb-2">
             Shipping & taxes are calculated at checkout
           </p>
