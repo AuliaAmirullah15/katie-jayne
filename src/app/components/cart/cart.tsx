e187b2967bcb0780d5deeb8a75d30fb2f679eb0e
Aulia Amirullah <auliaamir153@gmail.com>
2025-01-28 20:45:55 +0000

    Moved the empty cart to its own component

--- src/app/components/cart/cart.tsx
+++ src/app/components/cart/cart.tsx
@@ -12,7 +12,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import PrimaryButton, { ButtonType } from "../buttons/primaryButton";
 import { FaChevronDown } from "react-icons/fa";
 import { selectBasketSubtotal } from "@/app/stores/basketItemsSlice";
-import EmptyCartSVG from "@/app/components/cart/EmptyCartSVG";
+import EmptyCart from "./emptyCart";
 
 const ShoppingListItems: React.FC<{
   basketItem: BasketItem;
@@ -155,32 +155,6 @@ const Checkout: React.FC<{
     </>
   );
 };
-
-const EmptyCart: React.FC = () => (
-  <motion.div
-    initial={{ opacity: 0, y: 10 }}
-    animate={{ opacity: 1, y: 0 }}
-    className="flex flex-col items-center space-y-6 mt-4"
-  >
-    <EmptyCartSVG className="w-40 h-40 text-main_brown" />
-
-    <p className="text-lg text-gray-700 text-center">
-      Your shopping bag is currently empty. Start exploring our collection and
-      add your favorite items to the cart!
-    </p>
-
-    <PrimaryButton
-      type="button"
-      buttonType={ButtonType.Secondary}
-      className="px-6 py-2"
-      onClick={() => {
-        window.location.href = "/";
-      }}
-    >
-      Start Shopping
-    </PrimaryButton>
-  </motion.div>
-);
 
 const FilledCart: React.FC<{
   basketItems: BasketItem[];
---
+++ src/app/components/cart/emptyCart.tsx
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+import EmptyCartSVG from "./EmptyCartSVG";
+import PrimaryButton, { ButtonType } from "../buttons/primaryButton";
+
+export default function EmptyCart() {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      className="flex flex-col items-center space-y-6 mt-4"
+    >
+      <EmptyCartSVG className="w-40 h-40 text-main_brown" />
+
+      <p className="text-lg text-gray-700 text-center">
+        Your shopping bag is currently empty. Start exploring our collection and
+        add your favorite items to the cart!
+      </p>
+
+      <PrimaryButton
+        type="button"
+        buttonType={ButtonType.Secondary}
+        className="px-6 py-2"
+        onClick={() => {
+          window.location.href = "/";
+        }}
+      >
+        Start Shopping
+      </PrimaryButton>
+    </motion.div>
+  );
+}
