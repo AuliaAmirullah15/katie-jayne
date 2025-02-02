6ef400954552810e6185b1b2afc037e1097174e1
Aulia Amirullah <auliaamir153@gmail.com>
2025-02-02 18:38:17 +0000

    Added menu navbar and adjusted desktop menu height

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -7,14 +7,10 @@ import logo from "@/assets/images/svg/logo.svg";
 import magnifyingGlass from "@/assets/images/svg/magnifying_glass.svg";
 import heart from "@/assets/images/svg/heart.svg";
 import bag from "@/assets/images/svg/bag.svg";
-import unitedKingdom from "@/assets/images/svg/united_kingdom.svg";
 import { useSelector } from "react-redux";
 import { RootState } from "@/app/stores";
-import Overlay from "../layouts/overlay";
-import { menuItems, Menu, coreLinks } from "@/data/headerLinks";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import PrimaryButton, { ButtonType } from "../buttons/primaryButton";
+import { menuItems, Menu } from "@/data/headerLinks";
+import MobileNavbar from "./mobileNavbar";
 
 interface IconProps {
   src: string;
@@ -31,8 +27,8 @@ const MenuItem = ({ menu }: { menu: Menu }) => (
     </div>
     {/* Full-Screen Dropdown Menu */}
     {menu.children && (
-      <div className="fixed top-[84px] left-0 w-screen h-[calc(100vh-350px)] bg-white shadow-lg z-40 pl-2 flex opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity duration-300">
-        <div className="ml-60 mt-12 columns-1 sm:columns-2 md:columns-3 gap-12">
+      <div className="fixed top-[84px] left-0 w-screen bg-white shadow-lg z-40 pl-2 flex opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity duration-300">
+        <div className="ml-60 mt-12 mb-8 columns-1 sm:columns-2 md:columns-3 gap-12">
           {menu.children.map((item, index) => (
             <div key={index} className="flex flex-col mb-6 break-inside-avoid">
               <h2 className="text-black text-lg mb-4 font-semibold">
@@ -159,88 +155,7 @@ export default function Header() {
       </div>
 
       {/* Mobile Menu Navbar */}
-      <Overlay
-        isVisible={menuOpen}
-        className="lg:hidden"
-        onClose={() => setMenuOpen(false)}
-      >
-        <div className="flex flex-col justify-between h-full">
-          <div>
-            <div className="w-full flex flex-row items-center justify-center p-6 border-gray-200 border-b-2">
-              <Image
-                src={logo}
-                alt="Katie Jayne"
-                className="w-36 cursor-pointer "
-                onClick={handleLogoClick}
-              />
-            </div>
-            <nav className="space-y-4 text-gray-800 text-lg p-6 border-gray-200 border-b-2">
-              <ul className="flex flex-col space-y-4">
-                {menuItems.map((menu, key) => (
-                  <li
-                    key={key}
-                    className={`cursor-pointer hover:text-main_brown transition-all duration-300 ${menu.style}`}
-                  >
-                    <div className="flex justify-between items-center w-full">
-                      <span>{menu.label}</span>
-                      {/* Right arrow icon */}
-                      <span className="ml-2">
-                        <FontAwesomeIcon icon={faChevronRight} size="sm" />
-                      </span>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-            <div className="w-[full] m-4 flex flex-row items-center justify-center">
-              <div className="flex flex-col w-full md:w-[50%] items-center justify-center text-center">
-                <h4 className="text-lg mb-4">
-                  Become a Katie Jayne Member today for the best products,
-                  inspiration and offers.{" "}
-                  <span className="font-semibold underline">Learn More</span>
-                </h4>
-                <div className="flex flex-row items-center justify-center space-x-2">
-                  <PrimaryButton
-                    type="submit"
-                    className="w-auto md:mt-0 mx-0 text-md flex-auto"
-                    buttonType={ButtonType.Secondary}
-                  >
-                    Join Us
-                  </PrimaryButton>
-                  <PrimaryButton
-                    type="submit"
-                    className="w-auto md:mt-0 mx-0 text-md flex-auto"
-                    buttonType={ButtonType.Outlined}
-                  >
-                    Sign In
-                  </PrimaryButton>
-                </div>
-              </div>
-            </div>
-            <nav className="space-y-4 text-gray-800 text-lg px-6 py-4">
-              <ul className="flex flex-col space-y-3">
-                {coreLinks.map((link, key) => (
-                  <li
-                    key={key}
-                    className="cursor-pointer hover:text-main_brown transition-all duration-300"
-                  >
-                    <div className="flex flex-row justify-start w-full space-x-2">
-                      <Image src={link.icon} alt={link.label} width={24} />
-                      <span>{link.label}</span>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
-          <div className="flex flex-row space-x-3 left-0 w-full p-4 bg-white border-t border-gray-300">
-            <Image src={unitedKingdom} alt="UK Flag" width={30} height={30} />
-            <p className="text-lg hover:text-main_brown transition-all duration-300 hover:cursor-pointer">
-              United Kingdom
-            </p>
-          </div>
-        </div>
-      </Overlay>
+      <MobileNavbar isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
     </header>
   );
 }
