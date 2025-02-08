0f5cbf3c7123176b85236b3e0a085b28b3bb4816
Aulia Amirullah <auliaamir153@gmail.com>
2025-02-08 16:46:06 +0000

    Adjusted the gap

--- src/app/components/productListing/productListing.tsx
+++ src/app/components/productListing/productListing.tsx
@@ -20,7 +20,7 @@ const ProductListing = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-6">
         {productList.map((product, key) => (
           <div
             key={key}
