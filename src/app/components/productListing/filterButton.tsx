9f9f4554ab2a202a2d129cf923caeb481d4054bc
Aulia Amirullah <auliaamir153@gmail.com>
2025-02-08 15:22:13 +0000

    Adjusted the sort and filter button for smaller screen size

--- src/app/components/productListing/filterButton.tsx
+++ src/app/components/productListing/filterButton.tsx
@@ -3,8 +3,8 @@ import filter from "@/assets/images/png/filter.png";
 
 const FilterButton = () => {
   return (
-    <button className="flex flex-row space-x-2 text-sm border-black border-1 px-3 py-2 tracking-widest font-bold">
-      <span>FILTER & SORT</span>
+    <button className="flex flex-row md:space-x-2 text-sm border-black border-1 px-3 py-2 tracking-widest font-bold">
+      <span className="hidden md:block">FILTER & SORT</span>
       <span>
         <Image src={filter} alt="sort and filter" height={20} width={20} />
       </span>
