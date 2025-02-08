a7df54783df85d7de871618d7bce30f8afac924b
Aulia Amirullah <auliaamir153@gmail.com>
2025-02-08 13:04:21 +0000

    Broke up the components

--- src/app/components/productListing/productListing.tsx
+++ src/app/components/productListing/productListing.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import filter from "@/assets/images/png/filter.png";
-import { productFilters } from "@/data/headerLinks";
+import ProductFilters from "@/app/components/productListing/productFilters";
+import FilterButton from "@/app/components/productListing/filterButton";
 
 const ProductListing = () => {
   return (
@@ -8,30 +7,10 @@ const ProductListing = () => {
       <h2 className="text-2xl mb-4">Shop</h2>
       <div className="flex items-stretch justify-between border-gray-200 border-b-2">
         <div className="flex flex-row">
-          <ul className="flex flex-row space-x-10 text-gray-800 text-sm">
-            {productFilters.map((productFilter, key) => (
-              <li
-                key={key}
-                className="group relative cursor-pointer flex flex-row items-center justify-between h-full grow hover:text-main_brown duration-300 transition-all"
-              >
-                <span className="relative">{productFilter}</span>
-                <span className="absolute left-0 -bottom-1 w-0 h-[4px] bg-main_brown rounded-full transition-all duration-500 ease-in-out group-hover:w-full"></span>
-              </li>
-            ))}
-          </ul>
+          <ProductFilters />
         </div>
         <div className="flex flex-row mb-2">
-          <button className="flex flex-row space-x-2 text-sm border-black border-1 px-3 py-2 tracking-widest font-bold">
-            <span>FILTER & SORT</span>
-            <span>
-              <Image
-                src={filter}
-                alt="sort and filter"
-                height={20}
-                width={20}
-              />
-            </span>
-          </button>
+          <FilterButton />
         </div>
       </div>
     </div>
