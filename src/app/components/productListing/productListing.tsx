a16138cba8dcdbd97c236c15f02210b2b3d70473
Aulia Amirullah <auliaamir153@gmail.com>
2025-02-08 16:26:26 +0000

    Added grid view

--- src/app/components/productListing/productListing.tsx
+++ src/app/components/productListing/productListing.tsx
@@ -1,5 +1,6 @@
 import ProductFilters from "@/app/components/productListing/productFilters";
 import FilterButton from "@/app/components/productListing/filterButton";
+import { products } from "@/data/products";
 
 const ProductListing = () => {
   return (
@@ -17,6 +18,33 @@ const ProductListing = () => {
         <div className="hidden lg:flex flex-row items-center justify-center">
           <FilterButton />
         </div>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {products.map((product, key) => (
+          <div
+            key={key}
+            className="relative flex flex-col h-96 sm:h-50 group cursor-pointer"
+          >
+            <div
+              className="h-full relative bg-cover bg-center"
+              style={{ backgroundImage: `url(${product.image.src})` }}
+            >
+              <div className="absolute bottom-0 left-0 w-full opacity-0 group-hover:opacity-100 transition-all transform group-hover:translate-y-0 translate-y-6">
+                <div className="bg-white text-black text-sm py-4 px-4">
+                  <span>Buy Now</span>
+                </div>
+              </div>
+              <div className="absolute bottom-0 group-hover:rotate-[135deg] group-hover:pt-3 right-4 p-4 text-black opacity-100 transition-all transform group-hover:translate-y-0 group-hover:opacity-100">
+                <span className="text-xl">+</span>
+              </div>
+            </div>
+            <div className="p-4 text-gray-700">
+              <h3 className="text-sm">{product.name}</h3>
+              <p className="text-sm">£{product.price}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
