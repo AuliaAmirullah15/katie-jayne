d3628231f8972d3ceaed62165fcf63337a6631a7
Aulia Amirullah <auliaamir153@gmail.com>
2025-02-13 18:41:01 +0000

    Fixed the Applied Filters layout when there is no filter applied

--- src/app/components/productListing/productListing.tsx
+++ src/app/components/productListing/productListing.tsx
@@ -75,19 +75,25 @@ const ProductListing = () => {
       >
         <div className="flex-1 grow w-full overflow-y-auto">
           <div className="flex flex-col items-center justify-between mt-6">
-            <div className="flex flex-col space-y-4 mx-6 mb-4">
-              <h4 className="text-sm font-bold">APPLIED FILTERS</h4>
-              <div className="flex flex-row flex-wrap gap-2 w-full">
-                {selectedFilters.map((filter: Filter) => (
-                  <div
-                    key={filter.id}
-                    className="inline-flex items-center space-x-2 bg-gray-200 p-2 rounded max-w-max cursor-pointer"
-                    onClick={handleRemoveFilter(filter.id)}
-                  >
-                    <FontAwesomeIcon icon={faXmark} size="lg" />
-                    <p className="text-sm">{filter.name}</p>
-                  </div>
-                ))}
+            <div className="w-full">
+              <div className="flex flex-col space-y-4 mx-6 mb-4">
+                <h4 className="text-sm font-bold">APPLIED FILTERS</h4>
+                <div className="flex flex-row flex-wrap gap-2 w-full">
+                  {selectedFilters.length > 0 ? (
+                    selectedFilters.map((filter: Filter) => (
+                      <div
+                        key={filter.id}
+                        className="inline-flex items-center space-x-2 bg-gray-200 p-2 rounded max-w-max cursor-pointer"
+                        onClick={handleRemoveFilter(filter.id)}
+                      >
+                        <FontAwesomeIcon icon={faXmark} size="lg" />
+                        <p className="text-sm">{filter.name}</p>
+                      </div>
+                    ))
+                  ) : (
+                    <p className="text-gray-500">No Filter Applied</p>
+                  )}
+                </div>
               </div>
             </div>
             <div className="flex flex-col space-y-4 w-full">
