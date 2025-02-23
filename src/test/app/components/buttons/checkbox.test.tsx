0dfe71dcc04048c81b2b44722896c0d6d97d8641
Aulia Amirullah <auliaamir153@gmail.com>
2025-02-23 16:20:03 +0000

    Merged bask master

--- src/test/app/components/buttons/checkbox.test.tsx
+++ src/test/app/components/buttons/checkbox.test.tsx
@@ -3,44 +3,44 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import React from "react";
 
-describe("Checkbox Component", () => {
-  test("renders the checkbox container", () => {
+describe("Checkbox Component should", () => {
+  test("render the checkbox container", () => {
     render(<Checkbox isChecked={false} />);
     const checkboxContainer = screen.getByTestId("checkbox-label");
     expect(checkboxContainer).toBeInTheDocument();
   });
 
-  test("applies correct class when checked", () => {
+  test("apply correct class when checked", () => {
     render(<Checkbox isChecked={true} />);
     const checkboxLabel = screen.getByTestId("checkbox-label");
     expect(checkboxLabel).toHaveClass("bg-black");
   });
 
-  test("applies correct class when unchecked", () => {
+  test("apply correct class when unchecked", () => {
     render(<Checkbox isChecked={false} />);
     const checkboxLabel = screen.getByTestId("checkbox-label");
     expect(checkboxLabel).toHaveClass("bg-white");
   });
 
-  test("renders the checkmark when checked", () => {
+  test("render the checkmark when checked", () => {
     render(<Checkbox isChecked={true} />);
     const checkmark = screen.getByTestId("checkmark");
     expect(checkmark).toBeInTheDocument();
   });
 
-  test("does not render the checkmark when unchecked", () => {
+  test("not render the checkmark when unchecked", () => {
     render(<Checkbox isChecked={false} />);
     const checkmark = screen.queryByTestId("checkmark");
     expect(checkmark).not.toBeInTheDocument();
   });
 
-  test("sets aria-checked to true when checked", () => {
+  test("set aria-checked to true when checked", () => {
     render(<Checkbox isChecked={true} />);
     const checkboxInput = screen.getByRole("checkbox");
     expect(checkboxInput).toHaveAttribute("aria-checked", "true");
   });
 
-  test("sets aria-checked to false when unchecked", () => {
+  test("set aria-checked to false when unchecked", () => {
     render(<Checkbox isChecked={false} />);
     const checkboxInput = screen.getByRole("checkbox");
     expect(checkboxInput).toHaveAttribute("aria-checked", "false");
